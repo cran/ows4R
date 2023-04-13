eff14d9157baf710744027cf2222491fa9fe2888
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2023-04-13 14:10:02 +0000

    version 0.3-5

--- R/profile.R
+++ R/profile.R
@@ -1,5 +1,8 @@
 .onLoad <- function (libname, pkgname) { # nocov start
   
+  #make sure sf relies on... "sf" (aka ISO/OGC Simple Features)! and not s2
+  options(sf_use_s2 = FALSE)
+  
   ISOMetadataNamespace$GML$uri <- "http://www.opengis.net/gml"
   
   assign(".ows4R", new.env(), envir= asNamespace(pkgname))
